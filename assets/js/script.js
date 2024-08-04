b15dbe9e85f8964f7712edece64d278c6dee3ec0
Carlos Romao <carlosromao135@gmail.com>
2024-08-04 22:50:59 +0000

    Include constants for fixed parameters in Java

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// essential constants for build the script
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+const computerChoiceElement = document.getElementById('computer-choice');
+const resultElement = document.getElementById('result');
+
