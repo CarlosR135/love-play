2f27f8a7e3c1447683e12489e7e35ef3947709e9
Carlos Romao <carlosromao135@gmail.com>
2024-08-05 20:42:19 +0000

    Add click event handler for rock button to play the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,34 @@ const scissorsButton = document.getElementById('scissors');
 const computerChoiceElement = document.getElementById('computer-choice');
 const resultElement = document.getElementById('result');
 // function find the computer choice
-{
+
 function getComputerChoice() {
-    const choices = ['rock', 'paper','scissors'];
-    return choices[Math.floor(Math.random()* choices.length)];
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
-
-function determineWinner(playerChoice, computerChoice){
-    if (playerChoice === computerChoice)
+// function to determine the winner of the current round
+function determineWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
         return 'Tied!';
-}
-if (playerChoice === 'rock' && computerChoice === 'scissors'){
-    return 'You won!';
-}
-if (playerChoice === 'paper' && computerChoice === 'rock'){
-    return'You won!';
-}
-if (playerChoice === 'scissors' && computerChoice === 'paper'){
-    return'You lost!';
-}
+    }
+    if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return 'You won!';
+    }
+    if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return 'You won!';
+    }
+    if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return 'You lost!';
+    }
 
-return 'You lost!';
+    return 'You lost!';
 }
+//click event handler for rock button
+rockButton.addEventListener('click', () => {
+    const computerChoice = getComputerChoice();
+    const result = determineWinner('rock', computerChoice);
+    computerChoiceElement.textContent = `The computer chose: ${computerChoice}`;
+    resultElement.textContent = result;
+});
+
 
