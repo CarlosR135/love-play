ef2bd3a9ff11fb6f7f242567fc157c538fd199c9
Carlos Romao <carlosromao135@gmail.com>
2024-08-05 00:02:21 +0000

    Add functions for Rock, Paer, Scissors game: computer choice and result determination

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,27 @@ const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
 const computerChoiceElement = document.getElementById('computer-choice');
 const resultElement = document.getElementById('result');
+// function find the computer choice
+{
+function getComputerChoice() {
+    const choices = ['rock', 'paper','scissors'];
+    return choices[Math.floor(Math.random()* choices.length)];
+}
+
+function determineWinner(playerChoice, computerChoice){
+    if (playerChoice === computerChoice)
+        return 'Tied!';
+}
+if (playerChoice === 'rock' && computerChoice === 'scissors'){
+    return 'You won!';
+}
+if (playerChoice === 'paper' && computerChoice === 'rock'){
+    return'You won!';
+}
+if (playerChoice === 'scissors' && computerChoice === 'paper'){
+    return'You lost!';
+}
+
+return 'You lost!';
+}
 
